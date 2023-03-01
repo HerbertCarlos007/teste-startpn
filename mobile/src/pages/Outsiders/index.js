1584214b48152d469800d73effb1bf37fc46bd6c
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-03-01 15:15:12 -0300

    criando menu de configuração, busca e switch de troca

--- mobile/src/pages/Outsiders/index.js
+++ mobile/src/pages/Outsiders/index.js
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { Header } from '../../components/Header'
 import { styles } from './styles'
+import { EvilIcons } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 
 export const Outsiders = () => {
     return (
         <>
             <Header />
             <View style={styles.container}>
+                <View style={styles.topSection}>
+                    <View style={styles.leftSide}>
+                        <Text style={styles.title}>Terceiros</Text>
+                    </View>
+
+                    <View style={styles.righSide}>
+                        <TouchableOpacity>
+                            <View style={styles.containerActions}>
+                                <EvilIcons name="gear" size={24} color="#476EE6" />
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity>
+                            <View style={styles.containerActions}>
+                                <AntDesign name="search1" size={24} color="#476EE6" />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={styles.containerSwitchOutsider}>
+                    <TouchableOpacity>
+                        <View style={styles.containerClient}>
+                            <Text style={styles.textClient}>Clientes</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <View style={styles.containerSuppliers}>
+                            <Text style={styles.textSuppliers}>Fornecedores</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </View>
             </View>
         </>
     )
--- mobile/src/pages/Outsiders/styles.js
+++ mobile/src/pages/Outsiders/styles.js
@@ -3,6 +3,88 @@ import {StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
     container: {
         backgroundColor: '#FFFFFF', 
-        flex: 1
+        flex: 1,
+        paddingEnd: 16,
+        paddingStart: 16,
+    },
+
+    topSection: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 25,
+        alignItems: 'center'
+    },
+
+    righSide: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 20,
+        alignItems: 'center'
+        
+    },
+
+    title: {
+        fontSize: 25,
+        fontWeight: '600',
+        color: '#476EE6'
+    },
+
+    containerActions: {
+        width: 39,
+        height: 39,
+        backgroundColor: '#ffffff',
+        borderWidth: 1,
+        borderColor: '#D7D7D7',
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    containerSwitchOutsider: {
+        width: 380,
+        height: 45,
+        backgroundColor: '#ffffff',
+        borderWidth: 1,
+        borderColor: '#E8E8E8',
+        borderRadius: 100,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        alignItems: 'center',
+        paddingEnd: 10,
+        paddingStart: 10,
+    },
+
+    containerClient: {
+        width: 152,
+        height: 35,
+        backgroundColor: '#476EE6',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 100,
+    },
+
+    textClient: {
+        color: '#FFF',
+        fontSize: 15,
+        fontWeight: 500,
+    },
+
+    containerSuppliers: {
+        width: 152,
+        height: 35,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    },
+
+    textSuppliers: {
+        fontSize: 15,
+        fontWeight: 500,
+        color: '#476EE6'
     }
+
+    
 })
